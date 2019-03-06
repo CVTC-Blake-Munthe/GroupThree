4494a90cf06a3493fb2e295d7c56ccd0e94b8693
Eric <evandenheuvel1@student.cvtc.edu>
2019-03-06 13:44:30 -0600

    Broken

--- AgileTesting/Form1.Designer.cs
+++ AgileTesting/Form1.Designer.cs
@@ -28,26 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.shelterDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.shelterDataSet = new AgileTesting.ShelterDataSet();
+            this.FName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DayOne = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.DayTwo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day5 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day6 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day7 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day8 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day9 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day10 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day11 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day12 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day13 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day14 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day15 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day16 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day17 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day18 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day19 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Day20 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shelterDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shelterDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(312, 149);
+            this.button1.Location = new System.Drawing.Point(871, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FName,
+            this.LName,
+            this.DayOne,
+            this.DayTwo,
+            this.Day3,
+            this.Day4,
+            this.Day5,
+            this.Day6,
+            this.Day7,
+            this.Day8,
+            this.Day9,
+            this.Day10,
+            this.Day11,
+            this.Day12,
+            this.Day13,
+            this.Day14,
+            this.Day15,
+            this.Day16,
+            this.Day17,
+            this.Day18,
+            this.Day19,
+            this.Day20});
+            this.dataGridView1.DataSource = this.shelterDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1416, 594);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // shelterDataSetBindingSource
+            // 
+            this.shelterDataSetBindingSource.DataSource = this.shelterDataSet;
+            this.shelterDataSetBindingSource.Position = 0;
+            // 
+            // shelterDataSet
+            // 
+            this.shelterDataSet.DataSetName = "ShelterDataSet";
+            this.shelterDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // FName
+            // 
+            this.FName.HeaderText = "First Name";
+            this.FName.Name = "FName";
+            // 
+            // LName
+            // 
+            this.LName.HeaderText = "Last Name";
+            this.LName.Name = "LName";
+            // 
+            // DayOne
+            // 
+            this.DayOne.HeaderText = "1";
+            this.DayOne.Name = "DayOne";
+            // 
+            // DayTwo
+            // 
+            this.DayTwo.HeaderText = "2";
+            this.DayTwo.Name = "DayTwo";
+            // 
+            // Day3
+            // 
+            this.Day3.HeaderText = "3";
+            this.Day3.Name = "Day3";
+            // 
+            // Day4
+            // 
+            this.Day4.HeaderText = "4";
+            this.Day4.Name = "Day4";
+            // 
+            // Day5
+            // 
+            this.Day5.HeaderText = "5";
+            this.Day5.Name = "Day5";
+            // 
+            // Day6
+            // 
+            this.Day6.HeaderText = "6";
+            this.Day6.Name = "Day6";
+            // 
+            // Day7
+            // 
+            this.Day7.HeaderText = "7";
+            this.Day7.Name = "Day7";
+            // 
+            // Day8
+            // 
+            this.Day8.HeaderText = "8";
+            this.Day8.Name = "Day8";
+            // 
+            // Day9
+            // 
+            this.Day9.HeaderText = "9";
+            this.Day9.Name = "Day9";
+            // 
+            // Day10
+            // 
+            this.Day10.HeaderText = "10";
+            this.Day10.Name = "Day10";
+            // 
+            // Day11
+            // 
+            this.Day11.HeaderText = "11";
+            this.Day11.Name = "Day11";
+            // 
+            // Day12
+            // 
+            this.Day12.HeaderText = "12";
+            this.Day12.Name = "Day12";
+            // 
+            // Day13
+            // 
+            this.Day13.HeaderText = "13";
+            this.Day13.Name = "Day13";
+            // 
+            // Day14
+            // 
+            this.Day14.HeaderText = "14";
+            this.Day14.Name = "Day14";
+            // 
+            // Day15
+            // 
+            this.Day15.HeaderText = "15";
+            this.Day15.Name = "Day15";
+            // 
+            // Day16
+            // 
+            this.Day16.HeaderText = "16";
+            this.Day16.Name = "Day16";
+            // 
+            // Day17
+            // 
+            this.Day17.HeaderText = "17";
+            this.Day17.Name = "Day17";
+            // 
+            // Day18
+            // 
+            this.Day18.HeaderText = "18";
+            this.Day18.Name = "Day18";
+            // 
+            // Day19
+            // 
+            this.Day19.HeaderText = "19";
+            this.Day19.Name = "Day19";
+            // 
+            // Day20
+            // 
+            this.Day20.HeaderText = "20";
+            this.Day20.Name = "Day20";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 261);
+            this.ClientSize = new System.Drawing.Size(1440, 698);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shelterDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shelterDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +244,31 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource shelterDataSetBindingSource;
+        private ShelterDataSet shelterDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LName;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn DayOne;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn DayTwo;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day4;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day5;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day6;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day7;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day8;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day9;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day10;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day11;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day12;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day13;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day14;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day15;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day16;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day17;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day18;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day19;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Day20;
     }
 }
 
